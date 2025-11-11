198e7ea80488793b93039bf217864b59d7ebf663
Steve010300 <stleonard978@gmail.com>
2025-11-10 19:51:47 -0500

    did createEmployee seedEmployees function, seeded 10 initial employees

--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,9 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees (
+  id serial PRIMARY KEY,
+  name TEXT UNIQUE NOT NULL,
+  birthday DATE NOT NULL,
+  salary INTEGER NOT NULL  
+)
 
--- db/seed.js
+++ db/seed.js
@@ -1,4 +1,6 @@
 import db from "#db/client";
+import { faker } from "@faker-js/faker";
+import { createEmployee } from "./queries/employees.js";
 
 await db.connect();
 await seedEmployees();
@@ -6,5 +8,13 @@ await db.end();
 console.log("🌱 Database seeded.");
 
 async function seedEmployees() {
-  // TODO
+  for (let i = 0; i < 10; i++) {
+    const employee = {
+      name: faker.person.fullName(),
+      birthday: faker.date.birthdate({ min: 1960, max:2005, mode: "year" }).toISOString().split("T")[0],
+      salary: faker.number.int({ min: 40000, max: 150000 })
+    };
+
+    await createEmployee(employee);
+  }
 }
